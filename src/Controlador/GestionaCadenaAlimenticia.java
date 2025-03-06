86bba7d2ee4927d90d84c64e04c0c96598e7da15
DanielGM-34 <dgarmen630@g.educaand.es>
2025-03-06 09:59:36 +0100

    Hacer en casita2

--- src/Controlador/GestionaCadenaAlimenticia.java
+++ src/Controlador/GestionaCadenaAlimenticia.java
@@ -3,7 +3,11 @@ import Modelo.*;
 public class GestionaCadenaAlimenticia {
 
 	public static void main(String[] args) {
-		Animal2 f = new Serpiente(null, null, null, null, null);
+		Animal2 a1 = new Serpiente(null, null, null, null, null);
+		Animal2 a2 = new Rapaz(null, null, null, null, null);
+		Animal2 a3 = new Roedor(null, null, null, null, null);
+		Animal2 a4 = new Leon(null, null, null, null, null);
+
 	}
 
 }
